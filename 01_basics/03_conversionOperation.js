38c93c96472f43ebd54b453a13a12fcd0178056e
PRADEEP SINGH <97334070+PRADEEP-SINGH22@users.noreply.github.com>
2024-07-22 09:35:14 +0000

    conversionofdatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "22";
+
+console.log(typeof score);  //to know the type of score
+
+let valueinnumber = Number(score);
+
+console.log(typeof valueinnumber);
+
+let isloggedin = 0;
+
+let isloggedinchange = Boolean(isloggedin);
+
+console.log(isloggedinchange);
+
+let a = 3;
+
+let tostringa = String(a);
+
+console.log(tostringa);

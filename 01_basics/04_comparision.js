1eb913f6a3e3d7720c9460c373d560db7d135502
PRADEEP SINGH <97334070+PRADEEP-SINGH22@users.noreply.github.com>
2024-07-22 11:23:03 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+console.log( 2 << 16);
+
+console.log("3" >= 2); // compare krte waqt datatypes ka dhyaan rkhe
+
+console.log("2" == 2);
+
+console.log("2" === 2); // here strict checking is dono datatype is also checked 
+
+
+
+
+
+// node 01_basics/04_comparision.js

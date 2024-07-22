d37b4bb3d33ad3c01ad846ff9e66c1bf6f6e6dec
PRADEEP SINGH <97334070+PRADEEP-SINGH22@users.noreply.github.com>
2024-07-22 09:15:32 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountName = 121001
+let accountEmail = "pradeepch29@gmail.com"
+var accountPassword = "12345"
+accountCity = "faridabad"
+let accountstate
+
+// accountName = 2 constant change nhi hota hai
+
+accountEmail="pc@gmail.com"
+accountPassword = "54321"
+accountCity = "jalandhar"
+
+console.log(accountName);
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountstate  );
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+" use strict "; //for using newer version of js(code)
+
+// alert( 3*3 ) // we are use node js not browser (like html)
+
+console.log(3*3);  
+
+let name = " pradeep " // string
+let age = 23           // number
+let isloggedin = false //boolean
+
+//undefined mtlb sirf likh diya hai
+//null mtlb khali kuch bhi nhi 
+
+
+
+console.log(name);
+console.log(age);
+console.log(isloggedin);

7a79c2f8af7943582745afc88c629e0e41a726f9
PRADEEP SINGH <97334070+PRADEEP-SINGH22@users.noreply.github.com>
2024-07-22 09:49:56 +0000

    opertaions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,3 +17,35 @@ let a = 3;
 let tostringa = String(a);
 
 console.log(tostringa);
+
+// ***************************** operations ********************************************
+
+let val= 3;
+let negofval = -val;
+
+console.log(negofval);
+
+console.log(2**3);
+console.log(2%3);
+console.log(2*2);
+
+
+let s1= "Pradeep ";
+let s2= "  chaudhary";
+
+let s3=s1+s2;
+
+console.log(s3 );
+
+console.log("1" +2);
+
+console.log("1" +2 +2);
+
+console.log(1 +2 +"2");
+
+let s= 2;
+
+s++;
+
+console.log(s);
+

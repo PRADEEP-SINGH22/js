5e86bc0c85412006ff8cec72a1f7b68a1c4eeaaa
PRADEEP SINGH <97334070+PRADEEP-SINGH22@users.noreply.github.com>
2024-07-22 11:43:26 +0000

    datatypes-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,34 @@
+// primitive 
+
+// 7 types : string , number , boolean , null , undefined , symbol(unique bnane k liye ) , Bigint 
+
+  const a = 100;
+  const b = false;
+  let c = " nothing ";
+
+  let d = null;
+
+  let e;
+
+  const id = Symbol('123');
+
+  const bignumber = 1223443222223432;
+
+
+// reference ( non-primitive )
+
+// array , object , functions 
+
+const hero = ["batman","kaalki"]; //array
+
+{                           // object
+    name:"Pradeep "         
+    age: 23
+}
+  
+
+// function declaration
+const foo = function()
+{
+    console.log(" pardeep chaudhary ");
+}

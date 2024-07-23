a314a201129b065ee5e1d26f6fed0beb6077a871
PRADEEP SINGH <97334070+PRADEEP-SINGH22@users.noreply.github.com>
2024-07-23 09:56:03 +0000

    nums and math

---
+++ 01_basics/06_numandmath.js
@@ -0,0 +1,36 @@
+let a = 80;
+
+let b = new Number(80);
+
+console.log(a);
+
+console.log(b);
+
+console.log(a.toString());
+
+let aa = 326.6426763;
+
+console.log(aa.toFixed(2));
+console.log(aa.toPrecision(3));
+
+// *********************math**************************
+
+let p = -27;
+
+console.log( Math.abs(p));
+
+let pp = 27.8;
+
+console.log( Math.floor(pp));
+console.log( Math.ceil(pp));
+
+console.log(Math.random()*10 );
+
+const min = 10;
+const max=20;
+
+console.log((Math.random()*(max - min)) + min);
+
+
+
+

45c4bb4eec578236234bab1b3653ae8c0482bd7d
PRADEEP SINGH <97334070+PRADEEP-SINGH22@users.noreply.github.com>
2024-07-23 06:50:21 +0000

    stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -32,3 +32,36 @@ const foo = function()
 {
     console.log(" pardeep chaudhary ");
 }
+
+
+// ************************memeory**************************************
+
+// stack is for primitive type
+// heap memory is for non-primitive type
+
+let aa = "pradeep ";  //statically
+
+let bb = aa;
+
+bb = "chaudhary ";
+
+aa = " rahul"
+
+console.log(aa);
+console.log(bb);
+
+ //dynamically
+
+let user11 ={
+
+  email : "pradeepch",
+  password : " *****"
+}
+
+let user22 = user11;
+
+user22.email="pradeep";
+
+console.log(user22.email);
+console.log(user11.email);
+

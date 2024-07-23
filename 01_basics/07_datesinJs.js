7e8c47182748b63e1bc58f108033b370a0e24c7c
PRADEEP SINGH <97334070+PRADEEP-SINGH22@users.noreply.github.com>
2024-07-23 10:50:48 +0000

    dates

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -4,3 +4,8 @@ let mydate = new Date();
 console.log(mydate.toString());
 console.log(mydate.toDateString());
 console.log(mydate.toLocaleDateString());
+
+let createmydate = new Date(2024,7,18);
+
+console.log(createmydate.toLocaleDateString());
+

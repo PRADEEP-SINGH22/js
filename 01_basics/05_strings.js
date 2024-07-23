79e138e291c4a7f448bf32fc8eba1de9b754c7d1
PRADEEP SINGH <97334070+PRADEEP-SINGH22@users.noreply.github.com>
2024-07-23 08:50:46 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = " pradeep ";
+let repo = 50;
+
+console.log(name);
+
+console.log(name + repo + " extra");
+
+console.log( ` hello my name is ${name } and my repocount is ${repo}`);
+
+console.log(name[5]);
+
+console.log(name.length);
+
+console.log(name.toUpperCase());
+
+console.log(name.indexOf('p'));

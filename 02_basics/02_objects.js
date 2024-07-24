f9d9fc361b056d7a281e7757e30390022f9b0286
PRADEEP SINGH <97334070+PRADEEP-SINGH22@users.noreply.github.com>
2024-07-24 13:31:37 +0000

    objects

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,88 @@
+// node 02_basics/02_objects.js
+
+// object literals 
+
+const personal = {
+
+    name : " pradeep ",
+    roll : 232023022,
+    mobile : 92829297873,
+    email : " pradeepch@gmail.com",
+    age : 23
+}
+
+// console.log(personal);
+
+// console.log(personal.name);
+// console.log(personal["name"]);
+
+personal.name = " pradeep chaudhary ";
+
+// console.log(personal);
+
+// Object.freeze(personal);
+personal.age = 24;
+
+// console.log(personal);
+
+personal.foo = function()
+{
+    console.log(`this is my name, ${this.name}`);
+}
+
+// console.log(personal.foo());
+
+// singleton 
+
+const tinder = new Object();
+
+tinder.id = "12345";
+tinder.name = " rahul ";
+tinder.age = 26;
+
+// console.log(tinder);
+
+const customer = {
+
+    name: "happy ",
+    
+    address : {
+
+        city: "faridabad",
+        code: 121001
+    }
+}
+
+// console.log(customer.address);
+
+// const add = Object.assign(tinder,customer);
+const bdd = { ...tinder,...customer };
+
+// console.log( add);
+// console.log( bdd);
+
+const complete = [
+
+    {
+        id: 1,
+        islog: false
+    }
+
+    ,
+
+    {
+        id:2,
+        islog: true
+    }
+]
+
+console.log( complete[0]);
+
+
+
+
+
+
+
+
+
